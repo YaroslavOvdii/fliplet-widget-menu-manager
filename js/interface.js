984be25fa39234229c255eb7a24759aa2fd1c4af
Ricardo Seromenho <ricardo@seromenho.com>
2016-09-16 10:57:49 +0100

    WIP

--- js/interface.js
+++ js/interface.js
@@ -10,6 +10,9 @@
     return Handlebars.compile($('#template-' + name).html());
   }
 
+  var menusPromises = {};
+  var menusData = {};
+
   Fliplet.DataSources.get({ type: 'menu' })
     .then(function (dataSources) {
       if (dataSources.length === 0) {
@@ -19,6 +22,8 @@
       }
 
       dataSources.forEach(function (dataSource) {
+        menusPromises[dataSource.id] = [];
+        menusData[dataSource.id] = [];
         $('#select-menu').append(templates.menuOption(dataSource));
         $('#accordion').append(templates.menu(dataSource));
 
@@ -32,25 +37,103 @@
               return;
             }
 
+            console.log(dataSource.name + ' > Links: ', rows);
+
             rows.forEach(function (row) {
-              row.options = {
+              menusData[dataSource.id].push(row);
+              var selector = '[data-id='+ row.id +']  .link';
+              row.data.options = {
                 label: true
               };
 
               $('#menu-' + dataSource.id).append(templates.menuLink(row));
-              var linkActionProvider = Fliplet.Widget.open('com.fliplet.link', {
-                selector: '[data-id='+ row.id +']  .link',
-                data: {
-                  action: row
-                },
-              });
 
+              var linkProvider = Fliplet.Widget.open('com.fliplet.link', {
+                closeOnSave: false,
+                selector: selector,
+                data: row.data.action
+              });
+              linkProvider.row = row;
+              menusPromises[dataSource.id].push(linkProvider);
             });
-            console.log(rows);
           });
       });
 
-      console.log(dataSources);
+      console.log('Data Sources: ', dataSources);
     });
-})();
+  
+  // Listeners
+  $('#select-menu').on('change', function onMenuChange() {
+    // Change visible links
+    var menuId = $(this).val();
+    $('#accordion .menu').hide();
+    $('#menu-' + menuId).show();
+    
+    // Change menu name on input
+    var menuName = getSelectedMenuName();
+    setMenuName(menuName);
+  });
 
+  $('#save').on('click', function () {
+    // Get new data source name
+    var newMenuName = getMenuName();
+    var selectedMenuId = getSelectedMenuId();
+
+    if (!selectedMenuId) {
+      return;
+    }
+
+    var options = {
+      id: selectedMenuId,
+      name: newMenuName
+    };
+
+    // Update data source if name was changed
+    if (getSelectedMenuName() !== newMenuName) {
+      Fliplet.DataSources.update(options)
+        .then(function () {
+          setSelectedMenuName(newMenuName);
+        });
+    }
+
+    // Update Links
+
+    menusPromises[selectedMenuId].forEach(function (linkActionProvider) {
+      console.log('Row attached: ', linkActionProvider.row);
+
+      linkActionProvider.then(function (data) {
+        console.log('Data to save: ', data);
+        linkActionProvider.row.data.action = data.data;
+        linkActionProvider.row.data.order = 1;
+
+        Fliplet.DataSources.connect(selectedMenuId)
+          .then(function (source) {
+            return source.update(linkActionProvider.row)
+          });
+      });
+
+      linkActionProvider.forwardSaveRequest();
+    })
+  });
+
+  // Getters / Setters
+  function getSelectedMenuId() {
+    return $('#select-menu').val();
+  }
+
+  function getSelectedMenuName() {
+    return $('#select-menu option:selected').text();
+  }
+
+  function setSelectedMenuName(name) {
+    return $('#select-menu option:selected').text(name);
+  }
+
+  function getMenuName() {
+    return $('#menu-name').val();
+  }
+
+  function setMenuName(name) {
+    return $('#menu-name').val(name);
+  }
+})();
